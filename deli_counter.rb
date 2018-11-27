7ef0b5ed8887b43675c3975fac672619ccc8a1e3
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 15:45:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@ katz_deli = []
 
 def line(person_in_line)
   if person_in_line.length >= 1
+    current_line = "The line is currently: "
     person_in_line.each.with_index(1) do |person_in_line, index|
       puts "The line is currently: #{index}. #{person_in_line}"
     end
