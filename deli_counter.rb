c8f4a5c41c99d526a2e3ff582e45c1b50bda57a6
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 15:46:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(person_in_line)
   if person_in_line.length >= 1
     current_line = "The line is currently: "
     person_in_line.each.with_index(1) do |person_in_line, index|
-      puts "The line is currently: #{index}. #{person_in_line}"
+      current_line << "#{index}. #{person_in_line}"
     end
   elsif person_in_line.length < 1
     puts "The line is currently empty."
