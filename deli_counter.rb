72c373e5461b0d2fc6e0a4b0437a087ea90b7171
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 16:55:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,7 +29,7 @@ def now_serving(person_in_line)
   else
     current_customer = "Currently serving"
     person_in_line.each.with_index(1) do | person_in_line |
-      current_customer.pop # operator? "#{person_in_line}."
+      current_customer # operator? "#{person_in_line}."
     end
   puts # next person in line and removes them from the front
   end
