1a971de43da8a13b57dc392f54850edd36a54453
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 16:37:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 def take_a_number(number_in_line, person_in_line)
   current_number = "Welcome, "
   number_in_line.each.with_index(1) do |person_in_line, number_in_line|
-    current_number.reverse << " #{person_in_line}. You are number #{number_in_line} in line."
+    current_number << " #{person_in_line}. You are number #{number_in_line} in line."
   end
     puts current_number
   #need a .reverse b/c people count from 1, not zero
