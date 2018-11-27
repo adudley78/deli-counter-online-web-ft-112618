714c895920f335d8eac000ca7ea4b8723354e84d
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 15:55:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(person_in_line)
   else
     current_line = "The line is currently: "
     person_in_line.each.with_index(1) do |person_in_line, index|
-      current_line << " #{index}. #{person_in_line}"
+      current_line << "#{index}. #{person_in_line}"
     end
     puts current_line
   end
