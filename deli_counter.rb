dce5453681c7afa131b5b1e46d4d360832579eea
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 15:31:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(person_in_line)
+  if person_in_line.length >= 1
+    person_in_line.each.with_index(1) do |person_in_line, index|
+      puts "The line is currently: #{index}. #{person_in_line}"
+    end
+  elsif person_in_line.length < 1
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli_array, person_in_line)
+  puts #need a .reverse b/c people count from 1, not zero
+end
+
+def now_serving(person_in_line)
+
+  puts # next person in line and removes them from the front
+end
